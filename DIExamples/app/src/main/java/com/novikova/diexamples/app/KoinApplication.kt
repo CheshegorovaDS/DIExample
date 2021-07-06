7a5be3bd20026f237e5006e4994ce4d347d9ca83
dcheshegorova <peschink@mail.ru>
2021-07-06 11:28:31 +0600

    refactored module

--- DIExamples/app/src/main/java/com/novikova/diexamples/app/KoinApplication.kt
+++ DIExamples/app/src/main/java/com/novikova/diexamples/app/KoinApplication.kt
@@ -1,15 +1,11 @@
 package com.novikova.diexamples.app
 
 import android.app.Application
-import android.content.Context
-import android.content.SharedPreferences
 import com.novikova.diexamples.presentation.feature.di.featureModule
-import com.novikova.diexamples.store.LoggedManager
-import com.novikova.diexamples.store.LoggedProvider
+import com.novikova.diexamples.store.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
-import org.koin.dsl.module
 
 class KoinApplication: Application() {
 
@@ -21,16 +17,5 @@ class KoinApplication: Application() {
             androidContext(this@KoinApplication)
             modules(appModule, featureModule)
         }
-    }
-
-    private fun provideSharedPreferences(context: Context): SharedPreferences =
-            context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
-
-    private val appModule = module {
-        single<LoggedProvider> { LoggedManager(provideSharedPreferences(applicationContext)) }
-    }
-
-    companion object {
-        const val PREFS_NAME = "preferences_name"
     }
 }
--- DIExamples/app/src/main/java/com/novikova/diexamples/store/di/AppModule.kt
+++ DIExamples/app/src/main/java/com/novikova/diexamples/store/di/AppModule.kt
@@ -1,11 +1,18 @@
 package com.novikova.diexamples.store.di
 
+import android.content.Context
+import android.content.SharedPreferences
 import com.novikova.diexamples.store.LoggedManager
 import com.novikova.diexamples.store.LoggedProvider
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
-class AppModule {
-//    val appModule = module {
-//        single<LoggedProvider> { LoggedManager(get()) }
-//    }
-}
+    private fun provideSharedPreferences(context: Context): SharedPreferences =
+            context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+
+    val appModule = module {
+        single<LoggedProvider> { LoggedManager(provideSharedPreferences(androidContext())) }
+    }
+
+    const val PREFS_NAME = "preferences_name"
+
